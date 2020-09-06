a29b8d4b91ad09ecf47a9ab121d6eb68f4290cfa
Israilla <israillacaroline@hotmail.com>
2020-09-06 18:40:56 -0300

    Criacao dos arquivos de configuracao

---
+++ Curso/Curso/Data/Configurations/PedidoItemConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Data.Configurations
+{
+    class PedidoItemConfiguration : IEntityTypeConfiguration<PedidoItem>
+    {
+        public void Configure(EntityTypeBuilder<PedidoItem> builder)
+        {
+            builder.ToTable("PedidoItens");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Quantidade).HasDefaultValue().IsRequired();
+            builder.Property(p => p.Valor).IsRequired();
+            builder.Property(p => p.Desconto).IsRequired();
+        }
+    }
+}

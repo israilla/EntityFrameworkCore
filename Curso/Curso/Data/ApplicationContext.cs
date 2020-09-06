833531cf10282492964dbecf79ce558433868ffe
Israilla <israillacaroline@hotmail.com>
2020-09-06 17:30:50 -0300

    Criacao da classe de contexto

---
+++ Curso/Curso/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Curso.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        //config provider
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+        }
+    }
+}

44a3762ac1255f78262114982abec11676b31510
Israilla <israillacaroline@hotmail.com>
2020-09-06 19:20:10 -0300

    Criando a migration

--- Curso/Curso/Domain/Cliente.cs
+++ Curso/Curso/Domain/Cliente.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Curso.Domain
 {
+    [Table("Clientes")] // caso o nome da tabela seja diferente na base de dados
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required] //propriedade obrigatória
         public string Nome { get; set; }
+        [Column("Phone")] //caso o campo da tabela seja diferente na base de dados
         public string Telefone { get; set; }
         public string CEP {get;set;}
         public string Estado { get; set; }
